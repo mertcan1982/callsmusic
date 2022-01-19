71dde4bd6d0d91072194229bda2edd3278ae1444
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-01-19 21:37:33 +0200

    Update callsmusic.py

--- callsmusic/callsmusic/callsmusic.py
+++ callsmusic/callsmusic/callsmusic.py
@@ -22,7 +22,7 @@ def init_instance(chat_id: int):
         if queues.is_empty(chat_id):
             await stop(chat_id)
         else:
-            instance.input_filename = queues.get(chat_id)['file']
+            instance.input_filename = queues.get(chat_id)['dosya']
 
 
 def remove(chat_id: int):
@@ -43,7 +43,7 @@ def get_instance(chat_id: int) -> GroupCall:
 
 async def start(chat_id: int):
     await get_instance(chat_id).start(chat_id)
-    active_chats[chat_id] = {'playing': True, 'muted': False}
+    active_chats[chat_id] = {'çalıyor..': True, 'sustu': False}
 
 
 async def stop(chat_id: int):
@@ -62,42 +62,42 @@ async def set_stream(chat_id: int, file: str):
 def pause(chat_id: int) -> bool:
     if chat_id not in active_chats:
         return False
-    elif not active_chats[chat_id]['playing']:
+    elif not active_chats[chat_id]['çalıyor..']:
         return False
 
     get_instance(chat_id).pause_playout()
-    active_chats[chat_id]['playing'] = False
+    active_chats[chat_id]['çalıyor..'] = False
     return True
 
 
 def resume(chat_id: int) -> bool:
     if chat_id not in active_chats:
         return False
-    elif active_chats[chat_id]['playing']:
+    elif active_chats[chat_id]['çalıyor..']:
         return False
 
     get_instance(chat_id).resume_playout()
-    active_chats[chat_id]['playing'] = True
+    active_chats[chat_id]['çalıyor..'] = True
     return True
 
 
 def mute(chat_id: int) -> int:
     if chat_id not in active_chats:
         return 2
-    elif active_chats[chat_id]['muted']:
+    elif active_chats[chat_id]['sustu']:
         return 1
 
     get_instance(chat_id).set_is_mute(True)
-    active_chats[chat_id]['muted'] = True
+    active_chats[chat_id]['sustu'] = True
     return 0
 
 
 def unmute(chat_id: int) -> int:
     if chat_id not in active_chats:
         return 2
-    elif not active_chats[chat_id]['muted']:
+    elif not active_chats[chat_id]['sustu']:
         return 1
 
     get_instance(chat_id).set_is_mute(False)
-    active_chats[chat_id]['muted'] = False
+    active_chats[chat_id]['sustu'] = False
     return 0
